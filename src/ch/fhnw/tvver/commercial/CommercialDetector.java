2677706116899e62698fe93260876e77488594a4
Florian Bruggisser <florian@nexpose.ch>
2016-01-02 23:00:02 +0100

    implemented basic commercial recognition

--- src/ch/fhnw/tvver/commercial/CommercialDetector.java
+++ src/ch/fhnw/tvver/commercial/CommercialDetector.java
@@ -22,23 +22,105 @@ public class CommercialDetector extends AbstractDetector {
 	int counter = 0;
 
 	BufferedImage diffImage = null;
+
+	boolean commercial = false;
+
+	boolean training;
 	
 	@Override
 	protected void init(URLVideoSource videoSource, File storageDirectory, boolean training) {
+		training = true;
+		this.training = training;
 	}
 	
 	@Override
 	protected void process(VideoFrame videoFrame, float[] audio, List<Segment> result) {
 		// process video frame
-		Frame frame = videoFrame.getFrame();
 
 		if (videoFrame.isKeyframe()) {
 			out(counter + ": Key!");
 		}
 
+		if(training)
+		{
+			train(videoFrame);
+		}
+		else
+		{
+			double sameness = compareImages(diffImage, getFramePart(videoFrame.getFrame()));
+
+			//todo: set threshold
+			if(sameness < 0.9) {
+				if (!commercial) {
+					commercial = true;
+					System.out.println("Werbung ("+sameness + "): " + counter);
+					writeFrame(videoFrame, "comm_start");
+				}
+			}
+			else
+			{
+				if(commercial)
+				{
+					commercial = false;
+					System.out.println("Werbung ende ("+ sameness + "): " + counter);
+					writeFrame(videoFrame, "comm_end");
+				}
+			}
+		}
+
+		// create and add segment when commercial/non-commercial block is detected
+		Segment segment = new Segment(start, duration);
+		result.add(segment);
+		if (videoFrame.isLast()) {
+			// do some post-processing on segments here
+		}
+
+		if(counter == 100)
+		{
+			training = false;
+			try {
+				ImageIO.write(diffImage, "BMP", new File("frames/" + counter + "trained.bmp"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		counter++;
+	}
+
+	/***
+	 * Returns percentage value of sameness.
+	 * @param img1 Must be Diff Image (Main)
+	 * @param img2
+     * @return
+     */
+	double compareImages(BufferedImage img1, BufferedImage img2)
+	{
+		int sameCount = 0;
+		int blackCount = 0;
+
+		for (int v = 0; v < img1.getHeight(); v++) {
+			for (int u = 0; u < img1.getWidth(); u++) {
+				int img1Value = img1.getRGB(u, v);
+				int img2Value = img2.getRGB(u, v);
+
+				if(img1Value == Color.BLACK.getRGB()) {
+					blackCount++;
+					if (img1Value == img2Value) {
+						sameCount++;
+					}
+				}
+			}
+		}
+
+		return (double)sameCount / blackCount;
+	}
+
+	BufferedImage getFramePart(Frame frame)
+	{
 		//todo: calculate dynamically
-		int offsetX = 71;
-		int offsetY = 96;
+		int offsetX = 200;
+		int offsetY = 200;
 
 		BufferedImage image = new BufferedImage(offsetX, offsetY, BufferedImage.TYPE_INT_RGB);
 
@@ -65,6 +147,14 @@ public class CommercialDetector extends AbstractDetector {
 			}
 		}
 
+		return image;
+	}
+
+	void train(VideoFrame videoFrame)
+	{
+		Frame frame = videoFrame.getFrame();
+		BufferedImage image = getFramePart(frame);
+
 		if (diffImage == null) {
 			out("set diff image!");
 			diffImage = copyImage(image);
@@ -87,21 +177,14 @@ public class CommercialDetector extends AbstractDetector {
 			}
 		}
 
-		try {
-			ImageIO.write(image, "BMP", new File("frames/" + counter + "_aframe.bmp"));
-			ImageIO.write(diffImage, "BMP", new File("frames/" + counter + "_bdiff.bmp"));
-		} catch (IOException e) {
-			e.printStackTrace();
+		if(counter % 1000 == 0) {
+			try {
+				//ImageIO.write(image, "BMP", new File("frames/" + counter + "_aframe.bmp"));
+				ImageIO.write(diffImage, "BMP", new File("frames/" + counter + "_bdiff.bmp"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-
-		// create and add segment when commercial/non-commercial block is detected
-		Segment segment = new Segment(start, duration);
-		result.add(segment);
-		if (videoFrame.isLast()) {
-			// do some post-processing on segments here
-		}
-
-		counter++;
 	}
 
 	void out(Object obj)
@@ -120,6 +203,15 @@ public class CommercialDetector extends AbstractDetector {
 			return Color.WHITE.getRGB();
 		else
 			return Color.BLACK.getRGB();
+	}
+
+	void writeFrame(VideoFrame videoFrame, String name)
+	{
+		try {
+			videoFrame.getFrame().write(new File("frames/" + counter + "_" + name +".png"), Frame.FileFormat.PNG);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static BufferedImage copyImage(BufferedImage source){
